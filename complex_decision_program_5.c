aff1e8f156d85f8e6cc682a1324fc54e88c9b044
SELVAM D G <dasariselvam321@gmail.com>
2023-12-13 18:09:02 +0100

    'practice'

---
+++ complex_decision_program_5.c
@@ -0,0 +1,58 @@
+/*If the three sides of a triangle are entered through the keyboard, 
+write a program to check whether the triangle is isosceles, 
+equilateral, scalene or right angled triangle. */
+
+#include <stdio.h>
+float check_right_angle(float, float, float);
+
+int main(void)
+{
+    float side1, side2, side3;
+    char pass;
+
+    printf("Enter the three sides of the traingle separated by commas: ");
+    scanf("%f, %f, %f", &side1, &side2, &side3);
+
+    // equilater triangles have sides of equal length
+    (side1 == side2 == side3)? printf("Equilateral Triangle"): pass;
+
+    // isosceles traingles have atleast 2 sides of the triangles are equal
+
+    (side1 == side2 || side2 == side3 || side1 == side3) ? printf("Isosceles Traingle"): pass;
+
+    //right- angled triangles are the largest side square is equal to the sum of the other two side squares
+
+    if(check_right_angle(side1,side2,side3) == 0)
+        printf("Right-angled Triangle");
+    
+    return 0;
+}
+float check_right_angle(float a, float b, float c)
+{
+    float largest_side, rem_side1, rem_side2;
+    (a>b) ? ((a>c)? (largest_side = a): (largest_side = c)) : ((b>c)? (largest_side = b): (largest_side = c));
+
+    if(largest_side > a && largest_side > b)
+    {
+        rem_side1 = a;
+        rem_side2 = b;
+    }
+    else if(largest_side > b && largest_side > c)
+    {
+        rem_side1 = b;
+        rem_side2 = c;
+    }
+    else if(largest_side > a && largest_side > c)
+    {
+        rem_side1 = a;
+        rem_side2 = c;
+    }
+
+    if(largest_side *largest_side == ((rem_side1*rem_side1) + ( rem_side2 * rem_side2))) 
+        return 0 ;
+    
+    
+    return -1;
+
+
+}
