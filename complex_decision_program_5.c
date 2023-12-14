f0a45c29339dc34ee0d76e4d92b4778937c36edf
SELVAM D G <dasariselvam321@gmail.com>
2023-12-14 17:22:18 +0100

    update

--- complex_decision_program_5.c
+++ complex_decision_program_5.c
@@ -13,18 +13,24 @@ int main(void)
     printf("Enter the three sides of the traingle separated by commas: ");
     scanf("%f, %f, %f", &side1, &side2, &side3);
 
-    // equilater triangles have sides of equal length
-    (side1 == side2 == side3)? printf("Equilateral Triangle"): pass;
+    
 
     // isosceles traingles have atleast 2 sides of the triangles are equal
 
-    (side1 == side2 || side2 == side3 || side1 == side3) ? printf("Isosceles Traingle"): pass;
+    ((side1 == side2 && (side1 !=side3)) || side2 == side3 && (side1 !=side2) || side1 == side3 && (side2 !=side3)) ? printf("Isosceles Traingle"): pass;
+
+    // equilater triangles have sides of equal length
+    (side1 == side2 && side2 == side3 && side1 == side3)? printf("Equilateral Triangle"): pass;
 
     //right- angled triangles are the largest side square is equal to the sum of the other two side squares
 
     if(check_right_angle(side1,side2,side3) == 0)
         printf("Right-angled Triangle");
     
+    // scalene triangle has all three sides with different length and it is not right angled triangle
+
+    ((side1 != side2  && side2 != side3 && side1 != side3) && (check_right_angle(side1,side2,side3) != 0))? printf("Scalene Triangle"): pass;
+
     return 0;
 }
 float check_right_angle(float a, float b, float c)
